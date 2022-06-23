c01e9fc5933bd63ae465828879ae399ec323c9e2
Amanda Smith <ajs.amandasmith@gmail.com>
2022-06-22 19:18:30 -0500

    Renders the shows details when clicked

--- src/components/SelectedShowContainer.js
+++ src/components/SelectedShowContainer.js
@@ -3,6 +3,7 @@ import Episode from "./Episode";
 
 function SelectedShowContainer(props) {
   const selectedSeason = useState(1);
+  console.log('episodes', props.allEpisodes)
 
   function mapSeasons() {
     if (!!props.episodes) {
@@ -19,7 +20,7 @@ function SelectedShowContainer(props) {
   }
 
   function mapEpisodes() {
-    return props.episodes.map((e) => {
+    return props.allEpisodes.map((e) => {
       if (e.season == selectedSeason) {
         return <Episode eachEpisode={e} key={e.id} />;
       }
